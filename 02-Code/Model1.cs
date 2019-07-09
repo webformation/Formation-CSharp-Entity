e9d9d950efeaf93ea761440a3e48f740d5c09bf0
Dominique <dc@webformation.fr>
2019-07-09 17:27:18 +0200

    ajout code first vide

---
+++ 02-Code/Model1.cs
@@ -0,0 +1,42 @@
+﻿namespace _02_Code
+{
+    using System;
+    using System.Data.Entity;
+    using System.Linq;
+
+    public class Model1 : DbContext
+    {
+        // Votre contexte a été configuré pour utiliser une chaîne de connexion « Model1 » du fichier 
+        // de configuration de votre application (App.config ou Web.config). Par défaut, cette chaîne de connexion cible 
+        // la base de données « _02_Code.Model1 » sur votre instance LocalDb. 
+        // 
+        // Pour cibler une autre base de données et/ou un autre fournisseur de base de données, modifiez 
+        // la chaîne de connexion « Model1 » dans le fichier de configuration de l'application.
+        public Model1()
+            : base("name=Model1")
+        {
+        }
+
+        // Ajoutez un DbSet pour chaque type d'entité à inclure dans votre modèle. Pour plus d'informations 
+        // sur la configuration et l'utilisation du modèle Code First, consultez http://go.microsoft.com/fwlink/?LinkId=390109.
+
+        // public virtual DbSet<MyEntity> MyEntities { get; set; }
+        public virtual DbSet<Personne> Personnes { get; set; }
+    }
+
+    public class Personne
+    {
+        public int Id { get; set; }
+        public String nom { get; set; }
+        public int age { get; set; }
+        public override string ToString()
+        {
+            return nom + " " + age;
+        }
+    }
+    //public class MyEntity
+    //{
+    //    public int Id { get; set; }
+    //    public string Name { get; set; }
+    //}
+}
---
+++ 02-Code/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Code
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Model1 m = new Model1();
+            var liste = m.Personnes;
+            foreach(var p in liste)
+            {
+                Console.WriteLine(p);
+            }
+            Personne p1 = new Personne();
+            p1.nom = "Jean";
+            p1.age = 30;
+            m.Personnes.Add(p1);
+            m.SaveChanges();
+        }
+    }
+}
